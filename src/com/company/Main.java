dede369dddc2ec5395f3ed081b11d4dd3959141b
Hamza ALadaad <you@example.com>
2022-12-10 12:42:00 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,76 @@
+package com.company;
+
+
+    import java.util.Scanner;
+     import java .util.*;
+    import java .lang.System;
+   // import java.math.*;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // soru 10
+        // kullancı tarafından bir sayı girilecek,
+        // Eğer sıfırdan büyük ise  pozatif yazdır.
+        // Eğer sıfırdan küçük ise negatif yazdır .
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        if (a > 0) {     // true
+
+            System.out.println(" pozatif ");
+
+
+        } else {       // false
+
+            System.out.println(" negatif ");
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
